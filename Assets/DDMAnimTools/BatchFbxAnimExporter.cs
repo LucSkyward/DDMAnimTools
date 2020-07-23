05c8d77cf132f93b7d2cb27971bebeca53c50e1f
novia <novia@gmail.com>
2020-07-23 22:46:20 +0800

    init commit

---
+++ Assets/DDMAnimTools/BatchFbxAnimExporter.cs
@@ -0,0 +1,78 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BatchFbxAnimExporter : MonoBehaviour
+{
+    public const string ImportFolder = "FbxFiles";
+    public const string ExportFolder = "Mecanim";
+    
+    [MenuItem("DDMAnimTools/1.FbxToMecanim")]
+    public static void ExportFbxAnims()
+    {
+        ArrayList al = new ArrayList();
+        string[] fileEntries = Directory.GetFiles(Application.dataPath + "/" + ImportFolder, "*.fbx", SearchOption.AllDirectories);
+
+        foreach (string fileName in fileEntries)
+        {
+            int assetPathIndex = fileName.IndexOf("Assets");
+            string localPath = fileName.Substring(assetPathIndex);
+            
+            // Debug.LogError(localPath);
+
+            string clipName = Path.GetFileNameWithoutExtension(localPath) + ".anim";
+            string newpath = Path.GetDirectoryName(localPath).Replace(ImportFolder,ExportFolder);
+
+            // Debug.LogError(newpath);
+
+            if (!Directory.Exists(newpath))
+            {
+                Directory.CreateDirectory(newpath);
+            }
+
+            var allAssets = AssetDatabase.LoadAllAssetsAtPath(localPath);
+
+            foreach (var ass in allAssets)
+            {
+                if (ass is AnimationClip)
+                {
+                    AnimationClip oriClip = (AnimationClip) ass;
+
+                    //skip __preview__
+                    if (oriClip.name.StartsWith("__preview"))
+                    {
+                        continue;
+                    }
+
+                    //Save the clip
+                    AnimationClip copyClip = new AnimationClip();
+                    // if (!Resources.Load(newpath + "/" + clipName))
+                    // {
+                    EditorUtility.CopySerialized(oriClip, copyClip);
+                    AssetDatabase.CreateAsset(copyClip, newpath + "/" + oriClip.name + ".anim");
+
+                    // AnimationClip clip = Resources.Load<AnimationClip>(newpath + "/" + clipName);
+                    string conrollerPath = newpath + "/" + oriClip.name + ".controller";
+                    var controller = UnityEditor.Animations.AnimatorController.CreateAnimatorControllerAtPath(conrollerPath);
+                    var state = controller.AddMotion(copyClip);
+                    // state.AddExitTransition(false);
+
+                    string nameWithOutException = Path.GetFileNameWithoutExtension(oriClip.name);
+                    // Debug.LogError(newpath);
+                    string assetbundlePath = newpath.Replace("Assets\\" + ExportFolder + "\\", "Anim\\") + "\\" + nameWithOutException;
+                    // Debug.LogError(assetbundlePath);
+
+                    AssetImporter.GetAtPath(conrollerPath).SetAssetBundleNameAndVariant(assetbundlePath, "anim");
+                }
+            }
+            
+            // AnimationClip orgClip = (AnimationClip) AssetDatabase.LoadAssetAtPath(localPath, typeof(AnimationClip));
+
+            AssetDatabase.Refresh();
+        }
+    } 
+}
+#endif
---
+++ Assets/DDMAnimTools/BatchMecanimToDDMAnim.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_EDITOR
+
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildAnimAsset : MonoBehaviour
+{
+    [MenuItem("DDMAnimTools/2.MecanimToDDMAnim")]
+    public static void MecanimToDDMAnim()
+    {
+        string assetbundleBuildPath = Application.streamingAssetsPath;//Path.Combine(Application.streamingAssetsPath, "Anim/Test");
+        
+        FileUtil.DeleteFileOrDirectory(assetbundleBuildPath);
+        Directory.CreateDirectory(assetbundleBuildPath);
+        
+        BuildPipeline.BuildAssetBundles(assetbundleBuildPath, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+
+        AssetDatabase.Refresh();
+        
+        string[] fileEntries = Directory.GetFiles(assetbundleBuildPath, "*.*", SearchOption.AllDirectories);
+        foreach (var fi in fileEntries)
+        {
+            if (Directory.Exists(fi))
+            {
+                //ignore dir
+                continue;
+            }
+            if (!fi.EndsWith(".anim"))
+            {
+                FileUtil.DeleteFileOrDirectory(fi);
+            }
+        }
+        
+        AssetDatabase.Refresh();
+    }
+}
+#endif
